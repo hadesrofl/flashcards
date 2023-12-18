821831931e937164f79e7462a0d4de15f8ffce31
Rene Kremer <renejon90@googlemail.com>
2023-12-18 23:13:54 +0100

    style: Add background color and cards

--- src/app/flashcards/create/layout.tsx
+++ src/app/flashcards/create/layout.tsx
@@ -1,8 +1,13 @@
 import { CommonLayoutProps } from "@app/layout";
 import CenteredBox from "@components/CenteredBox";
+import Card from "@mui/material/Card";
 
 export default function FlashCardCreatePageLayout({
   children,
 }: CommonLayoutProps) {
-  return <CenteredBox width="1/2">{children}</CenteredBox>;
+  return (
+    <CenteredBox width="1/2">
+      <Card className="p-8">{children}</Card>
+    </CenteredBox>
+  );
 }
--- src/app/tags/layout.tsx
+++ src/app/tags/layout.tsx
@@ -1,7 +1,7 @@
 import { CommonLayoutProps } from "@app/layout";
-import { Box } from "@mui/material";
+import Card from "@mui/material/Card";
 import React from "react";
 
 export default function TagGalleryLayout({ children }: CommonLayoutProps) {
-  return <Box className="w-1/2">{children}</Box>;
+  return <Card className="w-1/2">{children}</Card>;
 }
