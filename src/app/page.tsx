4e4758358230b55ce09ac4d56d5dd8b87cf6eff6
Rene Kremer <renejon90@googlemail.com>
2024-01-11 21:51:29 +0100

    fix(Home): Always redirect to collections of cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
 import { redirect } from "next/navigation";
 import AppRoutes from "./appRoutes";
-import dbContext from "./api/_internal/shared/db/dbContext";
 
 export default async function Home() {
-  const flashCards = await dbContext.flashCards.list(undefined, 0, 1);
-  return redirect(
-    flashCards.length > 0
-      ? AppRoutes.flashCardRoutes.root
-      : AppRoutes.flashCardRoutes.create
-  );
+  return redirect(AppRoutes.flashCardRoutes.root);
 }
